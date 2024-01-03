11b35c718546f9d8b1f18c5d53a2aeb68df8e5da
Pontus Segervall <pontus.segervall@hotmail.se>
2024-01-03 08:37:22 +0100

    Secound commit

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,16 @@
-let dog = 0;
+const backpage = document.getElementById("popup");
+const openpage = document.getElementById("wrapper");
+if (openpage) {
+    openpage.addEventListener("click", function () {
+        openPopup();
+        console.log("dog");
+    });
+}
+function openPopup() {
+    backpage.style.display = "grid";
+    console.log("lul");
+}
+function closePopup() {
+    backpage.style.display = "grid";
+    console.log("lal");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-let dog = 0
+const backpage: HTMLElement | null = document.getElementById("popup");
+const openpage: HTMLElement | null = document.getElementById("wrapper");
+const book: HTMLElement | null = document.getElementById("wrapper");
+const backbtn: HTMLElement | null = document.getElementById("wrapper");
+
+if (openpage) {
+    openpage.addEventListener("click", function () {
+        openPopup();
+        console.log("dog")
+    });
+}
+
+function openPopup() {
+        backpage.style.display = "grid";
+          console.log("lul");
+}
+
+function closePopup() {
+        backpage.style.display = "grid";
+        console.log("lal");
+    
+}
